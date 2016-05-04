25412dfef59804be0cb9d0db7b73cada7e25950e
Jeff Crone <github@314ghz.com>
2016-05-04 17:12:20 -0400

    Added some functionality to the quiz template.

--- QuizApp/resources/views/takeQuiz.blade.php
+++ QuizApp/resources/views/takeQuiz.blade.php
@@ -4,7 +4,32 @@
         <title>Jeff's Quiz App</title>
     </head>
     <body>
-	Take Quiz with id {{ $id }}!
+	@if (isset($quiz) and !($quiz->isEmpty()))
+		<h1>{{$quiz->title}}</h1>
+		@forelse ($questions as $question)
+			@if ($question->quiz_id == $quiz->id)
+				{{$question->text}}
+				<br>
+				@forelse ($answers as $answer)
+					@if ($answer->question_id == $question->id)
+						<input type='checkbox' name='{{$question->id}}' value='{{$answer->id}}' id='q{{$question->id}}a{{$answer->id}}'>
+						<label for='q{{$question->id}}a{{$answer->id}}'>{{$answer->text}}</label>
+						<br>
+					@endif
+				@empty
+					No answers.
+					<br>
+					<br>
+				@endforelse
+			@endif
+		@empty
+			No questions.
+			<br>
+			<br>
+		@endforelse
+	@else
+		No quiz found.
+	@endif
 	<br>
 	<a href="/">Back to homepage</a>
     </body>
